fa92fb845789472ba1626fb1929c53fa4177df6a
Aitor Ibañez <aitiba@gmail.com>
2012-04-06 21:09:28 +0200

    faltaba subir los elements

---
+++ application/views/elements/header.php
@@ -0,0 +1,16 @@
+	<head>
+	<meta charset="utf-8">
+    <title>Layout and partial view example</title>
+		<style type="text/css">
+			body { margin: 10px 0; padding: 0; font: 14px Lucida Grande, Arial, sans-serif; text-align: center; }
+			.wrapper { text-align: left; margin: 0 auto; width: 500px; }
+			.logo { height: 80px; width: 140px; background: #eee; border: 1px solid #ddd; line-height: 80px; text-align: center; float: left; }
+			menu { float: left; padding: 0; margin: 66px 0 0 10px; }
+			menu ul { margin: 0; padding: 0; }
+			menu ul li { list-style-type: none; display: inline; display: inline-block;  }
+			#content { clear: both; padding-top: 10px; }
+			#content ul { margin: 0; padding: 0; }
+			#content ul li { list-style-type: none; }
+			footer { padding: 10px; background: #eee; }
+		</style>
+	</head>
